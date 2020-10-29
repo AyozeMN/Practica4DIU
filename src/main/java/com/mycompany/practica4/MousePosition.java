2c199b3e9b23855b31ab3864ff85f9fca4ad2221
Ayoze <ayoze.mesa102@alu.ulpgc.es>
2020-10-29 12:58:32 +0000

    v3

--- src/main/java/com/mycompany/practica4/MousePosition.java
+++ src/main/java/com/mycompany/practica4/MousePosition.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.practica4;
 
 /**
  *
- * @author Nicolas, Ayose
+ * @author Nicolas, Ayoze
  */
 public class MousePosition {
     private int x;
--- src/main/java/com/mycompany/practica4/lienzo.java
+++ src/main/java/com/mycompany/practica4/lienzo.java
@@ -1,23 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.practica4;
 
-import com.mycompany.practica4.MousePosition;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Iterator;
 import javax.swing.JPanel;
-import org.apache.commons.collections.buffer.CircularFifoBuffer;
 
 /**
  *
- * @author Nicolas
+ * @author Nicolas, Ayoze
  */
 public class lienzo extends JPanel {
-    
+    public Color colorB = Color.WHITE;
+    public Color colorP = Color.BLACK;
+
+    public void setColorB(Color colorB) {
+        this.colorB = colorB;
+    }
+
+    public void setColorP(Color colorP) {
+        this.colorP = colorP;
+    }
     
     private MousePosition fifo[] = new MousePosition[5];
     private int iter = 0;
@@ -26,23 +27,20 @@ public class lienzo extends JPanel {
             fifo[i] = new MousePosition(0,0);
         }
     }
+    
     @Override
     public void paintComponent(Graphics G){
         super.paintComponent(G);
-        this.setBackground(Color.WHITE);
-        this.setForeground(Color.BLACK);
-        G.drawLine(10,10,100,100);
+        this.setBackground(colorB);
+        this.setForeground(colorP);
         
         G.fillOval(fifo[0].getX(), fifo[0].getY(), 10, 10);
         G.fillOval(fifo[1].getX(), fifo[1].getY(), 10, 10);
         G.fillOval(fifo[2].getX(), fifo[2].getY(), 10, 10);
         G.fillOval(fifo[3].getX(), fifo[3].getY(), 10, 10);
-        G.fillOval(fifo[4].getX(), fifo[4].getY(), 10, 10);
-        
-        G.fillOval(200, 200, 10, 10);
-    
+        G.fillOval(fifo[4].getX(), fifo[4].getY(), 10, 10);    
     }
-    public void add(MousePosition a){
+    public void add(MousePosition a) throws InterruptedException{
         if (iter == 5){iter = 0;}
         fifo[iter] = a;
         iter++;
